f0c72f079a13edb7990310241a83db98c448eb8c
emugnier <emugnier@ucsd.edu>
2024-11-11 23:08:10 -0800

    fix the verify ci 2

--- tools/ci-setup-verus.sh
+++ tools/ci-setup-verus.sh
@@ -6,10 +6,10 @@ git clone \
   --depth 1 \
   --recurse-submodules --shallow-submodules \
   https://github.com/verus-lang/verus
+cd verus/source
 git fetch --depth 1 origin bc5b90f
 git checkout bc5b90f
 git submodule update --init --recursive --remote
-cd verus/source
 # trigger toolchain installation
 rustc --version
 echo "Verus is at $PWD"
